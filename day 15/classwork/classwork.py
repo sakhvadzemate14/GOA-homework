3f786385b2175d87683cec018e7b7247494688d0
sakhvadzemate14 <sakhvadzemate3@gmail.com>
2024-12-16 07:54:41 -0800

    Add files via upload

---
+++ day 15/classwork/classwork.py
@@ -0,0 +1,59 @@
+<<<<<<< HEAD
+#print(20 % 6 )
+# num=int(input("enter your num: "))
+
+# if num % 2==0:
+#     print("even")
+
+
+# else:
+#     print("odd")
+    
+    
+
+num=int(input("enter your num: "))
+if num%5==0:
+     print("ეს არის ხუთის ჯ ერა დი")
+else:
+     print("ეს არ არის ხუთის ჯერადი")     
+      
+ 
+
+
+
+
+    
+    #ნაშთიანი გაყოფა
+
+
+
+=======
+#print(20 % 6 )
+# num=int(input("enter your num: "))
+
+# if num % 2==0:
+#     print("even")
+
+
+# else:
+#     print("odd")
+    
+    
+
+num=int(input("enter your num: "))
+if num%5==0:
+     print("ეს არის ხუთის ჯ ერა დი")
+else:
+     print("ეს არ არის ხუთის ჯერადი")     
+      
+ 
+
+
+
+
+    
+    #ნაშთიანი გაყოფა
+
+
+
+>>>>>>> 8bf49274b65ebb4a3d2fc3df339420b04dd27c5b
---
+++ day 16/classwork/classwork.py
@@ -0,0 +1,33 @@
+<<<<<<< HEAD
+#for lettle  in range (125): #ამას ქვია საიტერაციო რიცხვი
+        #print("hello world "+ str(id))
+
+
+#range
+for  i in range(5):
+    print('goa best', i)
+
+for j in range(5):
+    print("Group 19", j)
+
+
+
+
+
+=======
+#for lettle  in range (125): #ამას ქვია საიტერაციო რიცხვი
+        #print("hello world "+ str(id))
+
+
+#range
+for  i in range(5):
+    print('goa best', i)
+
+for j in range(5):
+    print("Group 19", j)
+
+
+
+
+
+>>>>>>> 8bf49274b65ebb4a3d2fc3df339420b04dd27c5b
