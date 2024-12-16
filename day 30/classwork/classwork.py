1d4f0a1ac046d1ea70c7e7476d5f7f0e6f6103fc
sakhvadzemate14 <sakhvadzemate3@gmail.com>
2024-12-16 07:58:12 -0800

    Add files via upload

---
+++ day 30/classwork/classwork.py
@@ -0,0 +1,177 @@
+<<<<<<< HEAD
+#ფუნქციები -functions
+
+
+
+
+
+
+
+print()
+input()
+int()
+len()
+range()
+
+
+
+
+
+
+
+
+
+
+#define / def-განსაზღვრა
+
+def robertas_water():
+    print("გადადგი 4 ნაბიჯი წინ ")
+    print("შეუხვიე მარცხნივ")
+    print("გადადგი 2 ნაბიჯი წინ")
+    print("მოუშვი ონკანი")
+    print("შენგან ხელ მარცხნივ გამოიღე კარადა და აიღე გოას ჭიქა ")
+    print("დახურე კარადა")
+    print("მივიტანოთ ჭიქა")
+    print("მოუშვი ცივი წყალი")
+    print("მიუშვირე გოას  ჭიქა")
+    print("ავსეე წყალი ჭიქაში")
+    print("დაკეტე ონკანი")
+    print("შემობრუნდი")
+    print("გადმოდგი 2 ნაბიჯი წინ")
+    print("შებრუნდი მარჯვნივ")
+    print("გადმოდგი 4 წინ")
+    print ("მიდი მაგიდასთან")
+    print("დადგი ჭიქა მაგიდაზე")
+
+
+
+#საკლასო დავალება იქნება
+
+
+def plus():
+    num1=int(input("enter num1:"))
+    num2=int(input("enter num1:"))
+
+
+
+plus()
+
+
+
+
+
+
+
+
+#გაყოფა
+def division(a, b):
+    return a / b
+
+
+#გამრავლება
+ 
+
+def multiplication (a, b):
+    return a * b
+
+
+
+
+#მიმატება 
+def addition(a, b):
+    return a + b
+
+
+#გამოკლება 
+    def subtraction(a, b):
+        return a - b
+
+=======
+#ფუნქციები -functions
+
+
+
+
+
+
+
+print()
+input()
+int()
+len()
+range()
+
+
+
+
+
+
+
+
+
+
+#define / def-განსაზღვრა
+
+def robertas_water():
+    print("გადადგი 4 ნაბიჯი წინ ")
+    print("შეუხვიე მარცხნივ")
+    print("გადადგი 2 ნაბიჯი წინ")
+    print("მოუშვი ონკანი")
+    print("შენგან ხელ მარცხნივ გამოიღე კარადა და აიღე გოას ჭიქა ")
+    print("დახურე კარადა")
+    print("მივიტანოთ ჭიქა")
+    print("მოუშვი ცივი წყალი")
+    print("მიუშვირე გოას  ჭიქა")
+    print("ავსეე წყალი ჭიქაში")
+    print("დაკეტე ონკანი")
+    print("შემობრუნდი")
+    print("გადმოდგი 2 ნაბიჯი წინ")
+    print("შებრუნდი მარჯვნივ")
+    print("გადმოდგი 4 წინ")
+    print ("მიდი მაგიდასთან")
+    print("დადგი ჭიქა მაგიდაზე")
+
+
+
+#საკლასო დავალება იქნება
+
+
+def plus():
+    num1=int(input("enter num1:"))
+    num2=int(input("enter num1:"))
+
+
+
+plus()
+
+
+
+
+
+
+
+
+#გაყოფა
+def division(a, b):
+    return a / b
+
+
+#გამრავლება
+ 
+
+def multiplication (a, b):
+    return a * b
+
+
+
+
+#მიმატება 
+def addition(a, b):
+    return a + b
+
+
+#გამოკლება 
+    def subtraction(a, b):
+        return a - b
+
+>>>>>>> 8bf49274b65ebb4a3d2fc3df339420b04dd27c5b
